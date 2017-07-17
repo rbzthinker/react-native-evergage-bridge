12ccf9734039b7495a078de9a829d908b2125a4d
Erwin <nkaloli@gmail.com>
2017-07-17 22:13:03 +0800

    required fields (iOS)

--- ios/RNEvergage.h
+++ ios/RNEvergage.h
@@ -1,11 +1,13 @@
 
 #if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
+#import "RCTEventEmitter.h"
 #else
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 #endif
 
-@interface RNEvergage : NSObject <RCTBridgeModule>
+@interface RNEvergage : RCTEventEmitter <RCTBridgeModule>
 
 @end
   
