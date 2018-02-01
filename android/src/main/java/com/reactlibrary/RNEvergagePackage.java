129b4f7c142478d39ac6674ea4268d6f12af3693
rbzthinker <31064026+rbzthinker@users.noreply.github.com>
2018-02-01 17:39:03 +0700

    Handle Android RN 0.47 breaking changes

--- android/src/main/java/com/reactlibrary/RNEvergagePackage.java
+++ android/src/main/java/com/reactlibrary/RNEvergagePackage.java
@@ -17,7 +17,6 @@ public class RNEvergagePackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNEvergageModule(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
